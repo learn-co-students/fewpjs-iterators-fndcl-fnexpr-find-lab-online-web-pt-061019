f92d51a022a9600e3a9a31cd6fd781d612c8b0bc
Khris Punzalan <khrispunzalan@gmail.com>
2020-01-25 06:01:10 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(teamRecord) {
+  let win = teamRecord.find(year => year.result === "W")
+    // if(win){
+    //   return win.year
+    // } else {
+    //   return undefined
+    // }
+  return !!win ? win.year :undefined
 }
+
+// superbowlWin('KC Chiefs');
+
+// condition ? exprIfTrue : exprIfFalse
+//  conditional (ternary) operator is the only JavaScript operator that takes three operands: a condition followed by a question mark (?), then an expression to execute if the condition is truthy followed by a colon (:), and finally the expression to execute if the condition is falsy.
