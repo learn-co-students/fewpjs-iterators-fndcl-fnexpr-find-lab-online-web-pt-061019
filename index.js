4cd1b617cafcc35cca527c3b7e578d72f7dfa026
HuydDo <huydinhdo@yahoo.com>
2020-01-26 23:56:47 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let obj = record.find(o => o.result === "W" )
+  return obj ?  (obj.year) : undefined
+}
