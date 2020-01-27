5c1389943c6400478304b42acdcd4ad2ebf98c91
P0rtk3y <llama.p0rtk3y@gmail.com>
2020-01-26 22:05:13 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let winResult = record.find(year => year.result === "W")
+  return !!winResult ? winResult.year : undefined;
+}
