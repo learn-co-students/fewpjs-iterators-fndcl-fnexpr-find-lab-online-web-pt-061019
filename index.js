c2330ddea3e752c18b928a309991f277250cb098
Sam Markham <sam.e.markham@gmail.com>
2020-01-27 18:28:42 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+
+]
+
+function superbowlWin(arr) {
+  let win = arr.find( function(element) {
+    return element.result === "W";
+    // console.log(element.result);
+  })
+  if (win) {
+    return win.year;
+  }
+  // return win.year;
+}
