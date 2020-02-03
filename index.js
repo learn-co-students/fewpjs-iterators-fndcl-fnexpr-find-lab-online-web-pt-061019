808d9f305bf1e7724081de9bd09a7d17b0c8abdf
maasa333 <konomaasa@gmail.com>
2020-02-02 16:39:20 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "L"},
+//   {year: "2016", result: "N/A"}
+// ]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  let found = record.find(e => e.result === "W")
+  debugger
+  if (typeof found === "undefined") {
+    return undefined
+  } else {
+    return found.year
+  }
 }
+// superbowlWin(record)
