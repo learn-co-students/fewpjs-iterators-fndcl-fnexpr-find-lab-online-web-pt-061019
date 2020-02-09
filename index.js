fb31464328e5e219582d0fb18dcfca58c50d74f3
tshuttles <tshuttlesworth@gmail.com>
2020-02-09 17:40:23 -0600

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function superbowlWin(records) {
   const win = records.find( function(record) { 
     return record.result === "W"
   })
-  if (win !== undefined)
-    return win.year
-  else
-    return undefined
+  // if (win !== undefined)
+  //   return win.year
+  // else
+  //   return undefined
+  return win ? win.year : undefined 
 };
